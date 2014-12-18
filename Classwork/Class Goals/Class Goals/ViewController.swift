efe1ce4a5cdcfa0fa4b1b6ec5663cc8d06a87016
Andre <info@codedre.com>
2014-12-17 21:40:44 -0500

    Created my class goals app

---
+++ Classwork/Class Goals/Class Goals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Class Goals
+//
+//  Created by Andre Shonubi on 12/17/14.
+//  Copyright (c) 2014 Andre Shonubi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
