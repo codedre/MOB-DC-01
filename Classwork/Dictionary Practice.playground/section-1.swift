467efd2534ecda9af00d637a881156190b79237f
Andre <info@codedre.com>
2015-03-04 19:08:39 -0500

    update

---
+++ Classwork/Dictionary Practice.playground/section-1.swift
@@ -0,0 +1,113 @@
+//// Playground - noun: a place where people can play
+//
+//import UIKit
+//
+//var wayneGretzgy = ["firstName": "Wayne", "lastName": "Gretzgy", "team": "Capitals"]
+//
+//wayneGretzgy["firstName"]!
+//
+//if let waynename = wayneGretzgy["firstName"]{
+//    println(waynename)
+//}
+//
+//var ena: [String: Int] = [:]
+//
+//ena["legs"] = 4
+//ena["eyes"] = 2
+////ena["eyes"] = nil
+//ena["paws"] = 4
+//ena.removeValueForKey("paws")
+//
+//
+//for (key, value) in ena {
+//    println("Ena has \(value) \(key)")
+//}
+//
+//
+//for enaKey in ena.keys {
+//    println(enaKey)
+//}
+//
+//for enaValue in ena.values {
+//    println(enaValue)
+//}
+//
+//
+//var articles = [
+//    ["title": "Ena breaks tail", "date": "03/3/15", "author": "Andre"],
+//    ["title": "MOB class does Core Data", "date": "03/5/15", "author": "Rob"]
+//]
+//
+//articles[1]["title"]!
+//
+//for article in articles {
+//    println(article["title"]!)
+//}
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
